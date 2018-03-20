bef10b9dbae0f7f89d4c1d7f4ecc48bb27f274d6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-03-20 12:57:18 -0300

    Commit inicial

---
+++ wp-atom-integration.php
@@ -0,0 +1,22 @@
+<?php
+defined('ABSPATH') or die('No script kiddies please!');
+/*
+Plugin Name: Wordpress Access to Memory Integration
+Plugin URI:  https://github.com/IFRS/wp-atom-integration
+Description: Plugin para integração com o AtoM (Access to Memory).
+Version:     1.0.0
+Author:      Ricardo Moro
+Author URI:  https://github.com/ricardomoro
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+Text Domain: wp-atom-integration
+Domain Path: /lang
+*/
+
+register_activation_hook(__FILE__, function () {
+
+});
+
+register_deactivation_hook(__FILE__, function () {
+
+});
