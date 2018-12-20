c7c68e20ddb0e955b376e4a32b5b94a10ea4be71
Marcio Marchesini <marcio.marchesini@ifrs.edu.br>
2018-12-20 14:48:43 -0200

    Implementação do plugin.

--- shortcodes/atom-link.php
+++ shortcodes/atom-link.php
@@ -1,23 +1,110 @@
 <?php
 
+function atom_settings_admin_menu() { 
+
+	$page_title = 'Configuração do Atom';
+	$menu_title = 'Configuração do Atom';
+	$capability = 'manage_options';
+	$menu_slug  = 'atom-settings';
+	$function   = 'atom_settings_page';
+
+	add_plugins_page( $page_title,
+		$menu_title, 
+		$capability, 
+		$menu_slug, 
+		$function );
+
+	add_action( 'admin_init', 'update_atom_settings' );
+}
+
+function atom_settings_page(){
+	?>
+		<div class="wrap">
+			<h1 class='wp-heading-inline'>Configuração do Atom</h1>
+		</div>
+		<form method='post' action="options.php">
+			<?php
+				settings_fields( 'atom-settings' );
+				do_settings_sections( 'atom-settings' );
+			?>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">Url do index.php</th>
+					<td><input type="text" name="atom_url_index" value="<?php echo get_option('atom_url_index'); ?>"/></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Url base da API</th>
+					<td><input type="text" name="atom_url_api" value="<?php echo get_option('atom_url_api'); ?>"/></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Chave de acesso da API</th>
+					<td><input type="text" name="atom_api_key" value="<?php echo get_option('atom_api_key'); ?>"/></td>
+				</tr>
+			</table>
+			<?php submit_button(); ?>
+		</form>
+	<?php
+}
+
+function update_atom_settings() { 
+	register_setting( 'atom-settings', 'atom_url_index' );
+	register_setting( 'atom-settings', 'atom_url_api' );
+	register_setting( 'atom-settings', 'atom_api_key' );
+}
+
 function atom_link_shortcode( $atts, $content = '' ) {
     $arr = shortcode_atts(
         array(
-            'sf_culture' => 'pt'
+            'sf_culture' => 'pt',
+			'slug' => '',
+			'atom_url_index' => get_option('atom_url_index'),
+			'atom_url_api' => get_option('atom_url_api'),
+			'atom_api_key' => get_option('atom_api_key')
         ),
         $atts
     );
 
     wp_enqueue_script('atom-link-js');
     wp_localize_script('atom-link-js','shortcode_options', $arr);
-    wp_localize_script('atom-link-js','shortcode_slug', array('slug' => $content));
 
+    return '
+        <div id="atom-link-root">
+            <atom-link></atom-link>
+        </div>
+    ';
+
+    /*
     return '
         <div id="atom-link-root">
             <a v-if="!error" :href="result.digital_object.url">{{ result.title }}</a>
             <div v-else><p>Ocorreu um erro!</p></div>
         </div>
     ';
+    */
 }
 
-add_shortcode( 'atom-link', 'atom_link_shortcode' );
+function atom_link_recents_shortcode( $atts, $content = '' ) {
+    $arr = shortcode_atts(
+        array(
+            'sf_culture' => 'pt',
+			'number' => 5,
+			'atom_url_index' => get_option('atom_url_index'),
+			'atom_url_api' => get_option('atom_url_api'),
+			'atom_api_key' => get_option('atom_api_key')
+        ),
+        $atts
+    );
+
+    wp_enqueue_script('atom-link-js');
+    wp_localize_script('atom-link-js','shortcode_options', $arr);
+
+    return '
+        <div id="atom-link-root">
+            <atom-recents-link></atom-recents-link>
+        </div>
+    ';
+}
+
+add_action( 'admin_menu', 'atom_settings_admin_menu' );
+add_shortcode('atom-link', 'atom_link_shortcode' );
+add_shortcode('atom-recents-link', 'atom_link_recents_shortcode' );
--- src/atom-link.js
+++ src/atom-link.js
@@ -1,19 +1,226 @@
-//var Vue = require('vue');
 import Vue from 'vue';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://atom.ifrs.edu.br/api';
-axios.defaults.headers.common['REST-API-Key'] = 'b9a06bd3ab1cfd2a';
+var baseUrlAtom = shortcode_options.atom_url_index;
 
-new Vue({
-    el: '#atom-link-root',
-    data: {
-        error: true,
-        result: {}
+axios.defaults.baseURL = shortcode_options.atom_url_api;
+axios.defaults.headers.common['REST-API-Key'] = shortcode_options.atom_api_key;
+
+Vue.component('atom-link', {
+
+    template: '<table><tbody><tr class="active" v-for="item in items"><td :bgcolor=setBackgroundColor(item)><a :href=item.digital_object.url target="_blank">{{item.title}}</a></td></tr></tbody></table>',
+
+    data: function () {
+        return {
+            error: false,
+            items: []
+        }
     },
+
+    methods: {
+        setBackgroundColor(item) {
+            return this.$parent.setBackgroundColor(item);
+        },
+        setPending(value) {
+            this.$parent.setPending(value);
+        },
+        releasePending() {
+
+            if (this.$parent.releasePending()) {
+
+                app.tempItems.sort(function (a, b) {
+                    return a.reference_code < b.reference_code ? -1 : a.reference_code > b.reference_code ? 1 : 0;
+                });
+
+                if (app.tempItems.length) {
+                    app.tempItems[0].level = 1;
+                }
+                this.items = app.tempItems.slice();
+            }
+        },
+        addItemsBySlug(slug) {
+
+            if (!slug)
+                return;
+
+            let self = this;
+
+            axios.get('informationobjects/' + slug + '?sf_culture=' + shortcode_options.sf_culture)
+                .then(function (response) {
+
+                    self.setPending();
+
+                    if (response.data) {
+                        if (response.data.level_of_description.toUpperCase() == 'ITEM') {
+                            if (response.data.digital_object && response.data.digital_object.url) {
+                                self.error = false, app.tempItems.push(response.data);
+                            }
+                        } else if (response.data.reference_code) {
+                            self.addItemsByReferenceCode(response.data.reference_code);
+                        }
+                    }
+
+                    self.releasePending();
+                })
+                .catch(function (response) {
+                    self.error = true, app.tempItems = []
+                });
+        },
+        addItemsByReferenceCode(referenceCode) {
+
+            let self = this;
+
+            self.setPending();
+
+            axios.get('informationobjects?sq0=' + referenceCode + '&sf0=referenceCode&sf_culture=' + shortcode_options.sf_culture)
+                .then(function (response) {
+
+                    let ocorrencias = response.data.results;
+
+                    self.setPending(ocorrencias.length);
+
+                    ocorrencias.forEach(function (el) {
+                        if (el.level_of_description.toUpperCase() == 'ITEM') {
+                            el.level = 3;
+                            self.addItemsBySlug(el.slug);
+                        } else {
+                            el.level = 2;
+                            el.digital_object = {};
+                            el.digital_object.url = baseUrlAtom + el.slug;
+                            app.tempItems.push(el);
+                        }
+
+                        self.releasePending();
+                    });
+
+                    self.releasePending();
+                })
+                .catch(function (response) {
+                    self.error = true, self.title = null, self.atomUrl = shortcode_options.slug
+                });
+        }
+    },
+
     mounted() {
-        axios.get('/informationobjects/' + shortcode_slug.slug + '?sf_culture=' + shortcode_options.sf_culture)
-        .then(response => {this.error = false, this.result = response.data})
-        .catch(response => {this.error = true, this.result = response.data});
+
+        let url = shortcode_options.slug;
+
+        if (url) {
+
+            var splittedUrl = url.split('/');
+
+            if (splittedUrl.length) {
+
+                let slug = splittedUrl.pop();
+
+                this.addItemsBySlug(slug);
+
+            } else {
+
+                this.atomUrl = url;
+
+            }
+        }
+    }
+})
+
+Vue.component('atom-recents-link', {
+    template: '<table><tbody><tr class="active" v-for="item in items"><td :bgcolor=setBackgroundColor(item)><a :href=item.digital_object.url target="_blank">{{item.title}}</a></td></tr></tbody></table>',
+
+    data: function () {
+        return {
+            error: false,
+            items: []
+        }
+    },
+
+    methods: {
+        setBackgroundColor(item) {
+            return this.$parent.setBackgroundColor(item);
+        },
+        setPending(value) {
+            this.$parent.setPending(value);
+        },
+        releasePending() {
+
+            if (this.$parent.releasePending())
+                this.items = app.tempItems.slice();
+        },
+        addItems() {
+
+            let self = this;
+
+            axios.get('informationobjects?sort=lastUpdated&topLod=0&limit=' + shortcode_options.number + '&sf_culture=' + shortcode_options.sf_culture)
+                .then(function (response) {
+
+                    let ocorrencias = response.data.results;
+
+                    self.setPending();
+
+                    ocorrencias.forEach(function (el) {
+
+                        if (el.data && el.level_of_description.toUpperCase() == 'ITEM') {
+                            if (el.data.digital_object && el.data.digital_object.url) {
+                                el.level = 3;
+                                self.error = false, app.tempItems.push(el);
+                            }
+                        } else {
+                            el.level = 2;
+                            el.digital_object = {};
+                            el.digital_object.url = baseUrlAtom + el.slug;
+                            app.tempItems.push(el);
+                        }
+                    })
+
+                    self.releasePending();
+                })
+                .catch(function (response) {
+                    self.error = true, app.tempItems = []
+                });
+        }
+    },
+
+    mounted() {
+
+        if (!shortcode_options.number)
+            shortcode_options.number = 20;
+
+        this.addItems();
+    }
+})
+
+var app = new Vue({
+
+    el: '#atom-link-root',
+
+    data: {
+        callPending: 0,
+        tempItems: []
+    },
+
+    methods: {
+        setPending(value) {
+            if (!value) this.callPending++;
+            else this.callPending += value;
+        },
+        releasePending() {
+
+            let self = this;
+
+            if (this.callPending > 0)
+                this.callPending--;
+
+            return this.callPending == 0;
+        },
+        setBackgroundColor(item) {
+            if (item.level) {
+                if (item.level == 1)
+                    return '#00b300';
+                if (item.level == 2)
+                    return '#00cc00';
+                else
+                    return '#ccffcc';
+            }
+        },
     }
 });
